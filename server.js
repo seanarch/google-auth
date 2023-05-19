6df55e3ee7b10bdc4f477b2e87ba9d46f6fd058a
seanarch <zhuqingyun@outlook.com>
2023-05-18 21:40:36 -0600

    Implement cookie-session

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const https = require("https");
 const express = require("express");
 const helmet = require("helmet");
 const passport = require("passport");
+const cookieSession = require("cookie-session");
 const { Strategy } = require("passport-google-oauth20");
-
-require("dotenv").config();
+const { verify } = require("crypto");
 
 require("dotenv").config();
 
@@ -15,6 +15,8 @@ const PORT = 3000;
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 };
 
 const AUTH_OPTIONS = {
@@ -33,10 +35,18 @@ passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 const app = express();
 
 app.use(helmet());
+
+app.use(
+  cookieSession({
+    name: "session",
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+  })
+);
 app.use(passport.initialize());
 
 function checkLoggedIn(req, res, next) {
-  console.log("Current user is:", req.user);
+  console.log("Current user is: ", req.user);
   const isLoggedIn = true;
   if (!isLoggedIn) {
     return res.status(401).json({
