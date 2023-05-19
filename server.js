2d68aa00f997469238129f4fbc48c357796c0087
seanarch <zhuqingyun@outlook.com>
2023-05-18 20:22:28 -0600

    Configure env, create https server

--- server.js
+++ server.js
@@ -3,47 +3,10 @@ const path = require("path");
 const https = require("https");
 const express = require("express");
 const helmet = require("helmet");
-const passport = require("passport");
-const { Strategy } = require("passport-google-oauth20");
-const cookieSession = require("cookie-session");
-const { verify } = require("crypto");
 
 require("dotenv").config();
 
 const PORT = 3000;
-
-const config = {
-  CLIENT_ID: process.env.CLIENT_ID,
-  CLIENT_SECRET: process.env.CLIENT_SECRET,
-  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
-  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
-};
-
-const AUTH_OPTIONS = {
-  callbackURL: "/auth/google/callback",
-  clientID: config.CLIENT_ID,
-  clientSecret: config.CLIENT_SECRET,
-};
-
-function verifyCallback(accessToken, refreshToken, profile, done) {
-  console.log("Google profile", profile);
-  done(null, profile);
-}
-
-passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
-
-// Save the session to the cookie
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-// Read the session from the cookie
-passport.deserializeUser((id, done) => {
-  // User.findById(id).then(user => {
-  //   done(null, user);
-  // });
-  done(null, id);
-});
 
 const app = express();
 
@@ -60,6 +23,28 @@ function checkLoggedIn(req, res, next) {
   next();
 }
 
+app.get("/auth/google", (req, res) => {});
+
+app.get("/auth/google/callback", (req, res) => {});
+
+app.get("/auth/logout", (req, res) => {});
+
 app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Your personal secret value is 42!");
 });
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`);
+  });
